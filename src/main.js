73d61a3929e35b674119a6d3f756dca1716c3bc3
斯兔 <yq5858588>
2018-06-03 14:12:42 +0800

    no commit message

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 const electron = require('electron');
-const {dialog} = require('electron');
-const {shell} = require('electron');
+const { dialog } = require('electron');
+const { shell } = require('electron');
 
 const ipc = electron.ipcMain;
 
@@ -21,41 +21,49 @@ let appIcon = null;
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
+/*
+console.log(app.getPath('pepperFlashSystemPlugin'));
+
+app.commandLine.appendSwitch('ppapi-flash-path',app.getPath('pepperFlashSystemPlugin'));
+app.commandLine.appendSwitch('ppapi-flash-version', '29.0.0.013');*/
 
 function createWindow() {
 
     // Create the browser window.
     mainWindow = new BrowserWindow({
         transparent: false,
-        frame: true,
+        frame: false,
         width: 779,
         fullscreenable: true,
         height: 558,
-        alwaysOnTop:true,
-        icon: path.join(__dirname, '../res/icon.ico')
+        alwaysOnTop: true,
+        icon: path.join(__dirname, '../res/icon.ico'),
+        'webPreferences': {
+            'plugins': true
+        }
     })
 
     mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname, '1.html'),
+        pathname: path.join(__dirname, 'index.html'),
         protocol: 'file:',
         slashes: true
     }))
 
     appIcon = new Tray(path.join(__dirname, '../res/icon.ico'));
     var contextMenu = Menu.buildFromTemplate([{
-        label: '最大化',
-        type: 'normal',
-        role: 'close',
-        click: function (item, focusedWindow) {
-            mainWindow.maximize();
-            mainWindow.show();
-        }
-    },
+            label: '最大化',
+            type: 'normal',
+            role: 'close',
+            click: function(item, focusedWindow) {
+                mainWindow.maximize();
+                mainWindow.show();
+            }
+        },
         {
             label: '最小化',
             type: 'normal',
             role: 'minimize',
-            click: function (item, focusedWindow) {
+            click: function(item, focusedWindow) {
                 mainWindow.hide();
             }
         },
@@ -63,16 +71,16 @@ function createWindow() {
             label: '关于',
             type: 'normal',
             checked: true,
-            click: function (item, focusedWindow) {
+            click: function(item, focusedWindow) {
                 shell.openExternal("https://gitee.com/yq5858588")
             }
         },
-        {label: '', type: 'separator'},
+        { label: '', type: 'separator' },
         {
             label: '退出',
             type: 'normal',
             role: 'close',
-            click: function (item, focusedWindow) {
+            click: function(item, focusedWindow) {
                 app.quit();
             }
         }
@@ -80,7 +88,7 @@ function createWindow() {
     appIcon.setToolTip('欢迎使用数据库管理软件');
     appIcon.setContextMenu(contextMenu);
     //单点击 1.主窗口显示隐藏切换 2.清除闪烁
-    appIcon.on("click", function () {
+    appIcon.on("click", function() {
         // if (!!timer) {
         // appIcon.setImage(path.join(appIcon, 'icon.ico'))
         //主窗口显示隐藏切换
@@ -91,7 +99,7 @@ function createWindow() {
     // Open the DevTools.
     // mainWindow.webContents.openDevTools()
     //注册打开控制台的快捷键
-    globalShortcut.register('F7', function () {
+    globalShortcut.register('F7', function() {
         let win = BrowserWindow.getFocusedWindow();
         if (win) {
             // win.webContents.print();
@@ -104,7 +112,7 @@ function createWindow() {
         }
     });
     //快捷键
-    globalShortcut.register('F6', function () {
+    globalShortcut.register('F6', function() {
         let win = BrowserWindow.getFocusedWindow();
         if (!win) return;
         win.reload();
@@ -121,7 +129,7 @@ function createWindow() {
         });
         if (num !== 0) event.preventDefault();*/
     });
-    mainWindow.on('closed', function (event) {
+    mainWindow.on('closed', function(event) {
         mainWindow = null
     })
 }
@@ -139,7 +147,7 @@ if (shouldQuit) {
 }
 app.on('ready', createWindow)
 
-app.on('window-all-closed', function (event) {
+app.on('window-all-closed', function(event) {
     if (appIcon) appIcon.destroy();
     if (process.platform !== 'darwin') {
         app.quit()
@@ -147,24 +155,24 @@ app.on('window-all-closed', function (event) {
 
 })
 
-app.on('activate', function () {
+app.on('activate', function() {
     if (mainWindow === null) {
         createWindow();
     }
 })
 //登录窗口最小化
-ipc.on('window-min', function () {
+ipc.on('window-min', function() {
     mainWindow.minimize();
 })
 //登录窗口最大化
-ipc.on('window-max', function () {
+ipc.on('window-max', function() {
     if (mainWindow.isMaximized()) {
         mainWindow.restore();
     } else {
         mainWindow.maximize();
     }
 })
-ipc.on('window-close', function () {
+ipc.on('window-close', function() {
     mainWindow.close();
 })
 // In this file you can include the rest of your app's specific main process
