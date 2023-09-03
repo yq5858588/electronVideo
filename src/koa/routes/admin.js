13ff16eb4fa4be7db8261f0e9e3d6b22aca6328f
393978119@qq.com <yq5858588>
2023-09-03 15:23:52 +0800

    更新文件

--- src/koa/routes/admin.js
+++ src/koa/routes/admin.js
@@ -1,16 +1,16 @@
 // const DB = require('../module/mongodb');
 const router = require('koa-router')();
 router
-  .get('/', async (ctx) => {
-      let title = "sdfasdfasd";
-      ctx.body = "";
-      var res = await DB.find('user', {});
-      // console.log(res);
-      await ctx.render('index', {
-          title: title,
-          list: res
-      });
-  })
+    .get('/', async (ctx) => {
+        let title = "sdfasdfasd";
+        ctx.body = "";
+        var res = await DB.find('user', {});
+        // console.log(res);
+        await ctx.render('index', {
+            title: title,
+            list: res
+        });
+    })
 var user = require('./admin/user');
 var node = require('./admin/node');
 var index = require('./admin/index');
